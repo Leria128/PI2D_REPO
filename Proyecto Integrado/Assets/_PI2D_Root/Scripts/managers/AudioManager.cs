263f9ffe0e43dba22892a8464f3c56266ed60ba3
Claramh-13 <claramonasterioh@gmail.com>
2026-02-12 11:50:03 +0100

    proceso de audio manager

--- Proyecto Integrado/Assets/_PI2D_Root/Scripts/managers/AudioManager.cs
+++ Proyecto Integrado/Assets/_PI2D_Root/Scripts/managers/AudioManager.cs
@@ -1,16 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  
+   public static AudioManager instance;
+
+    [Header("Audio Source References")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("Audio Clip Arrays")]
+    public AudioClip[] musicList;
+    public AudioClip[] sfxList;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;    
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayMusic(int musicIndex)
     {
         
+        musicSource.clip = musicList[musicIndex];
+        musicSource.Play();
     }
+
+    public void PlaySFX(int sfxIndex)
+    {
+        sfxSource.PlayOneShot(sfxList[sfxIndex]);
+    }
+
 }
