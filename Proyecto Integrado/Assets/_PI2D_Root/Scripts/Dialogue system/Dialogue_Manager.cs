206e636fe7f7b6a6ab20f9dda7c245255369411b
Vleriia <valeriaballmar@gmail.com>
2026-02-12 16:32:21 +0100

    beep beep

--- Proyecto Integrado/Assets/_PI2D_Root/Scripts/Dialogue system/Dialogue_Manager.cs
+++ Proyecto Integrado/Assets/_PI2D_Root/Scripts/Dialogue system/Dialogue_Manager.cs
@@ -9,74 +9,52 @@ namespace DialogueSystem_V
     public class DialogueManager : MonoBehaviour
     {
         public static DialogueManager Instance { get; private set; }
-
+        [SerializeField] private Dialogue_UI dialogueUI;
         private Queue<Dialogue_Turn> dialogueTurnsQueue;
+
+        public bool IsDialogInProgress { get; private set; } = false;
 
         private void Awake()
         {
             Instance = this;
-            HideDialogBox();
+            dialogueUI.HideDialogBox();
         }
 
         public void StartDialogue(Dialogue_Round dialogue)
         {
+            if (IsDialogInProgress) 
+            {
+                Debug.LogWarning($"Dialogue already in progress");
+                return;
+            }
+
+            IsDialogInProgress = true;
             dialogueTurnsQueue = new Queue<Dialogue_Turn>(dialogue.Dialogue_Turns);
             StartCoroutine(DialogueCoroutine());
         }
 
         private IEnumerator DialogueCoroutine()
         {
-            ShowDialogBox();
+            dialogueUI.ShowDialogBox();
 
             while (dialogueTurnsQueue.Count > 0)
             {
                 var currentTurn = dialogueTurnsQueue.Dequeue();
 
-                SetCharacterInfo(currentTurn.Character, GetCharacterPhoto());
-                ClearDialogueArea();
-
-                dialogArea.text = currentTurn.Dialogue_Line;
+                dialogueUI.SetCharacterInfo(currentTurn.Character);
+                dialogueUI.ClearDialogueArea();
+                dialogueUI.SetDialogArea(currentTurn.Dialogue_Line);
+                
 
                 yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
                 yield return null;
             }
 
-            HideDialogBox();
+            dialogueUI.HideDialogBox();
+            IsDialogInProgress = false;
         }
 
-        // Dialogue UI
-        [SerializeField] private RectTransform dialogBox;
-        [SerializeField] private Image characterPhoto;
-        [SerializeField] private TextMeshProUGUI characterName;
-        [SerializeField] private TextMeshProUGUI dialogArea;
-
-        public void ShowDialogBox()
-        {
-            dialogBox.gameObject.SetActive(true);
-        }
-
-        public void HideDialogBox()
-        {
-            dialogBox.gameObject.SetActive(false);
-        }
-
-        public Image GetCharacterPhoto()
-        {
-            return characterPhoto;
-        }
-
-        public void SetCharacterInfo(Dialogue_Character character, Image characterPhoto)
-        {
-            if (character == null) return;
-
-            characterPhoto.sprite = character.Photo;
-            characterName.text = character.Name;
-        }
-
-        public void ClearDialogueArea()
-        {
-            dialogArea.text = string.Empty;
-        }
+        
     }
 }
 
